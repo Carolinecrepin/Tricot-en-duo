75f3720960d74d0525c1335f928d11385f6615a9
caroline <crepin.caroline@gmail.com>
2022-07-30 14:13:06 +0200

    order detail account

--- src/Controller/Account/AccountController.php
+++ src/Controller/Account/AccountController.php
@@ -2,16 +2,33 @@
 
 namespace App\Controller\Account;
 
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Order;
 
 #[Route('/account')]
 class AccountController extends AbstractController
 {
     #[Route('/', name: 'account')]
-    public function index(): Response
+    public function index(OrderRepository $orderRepo): Response
     {
-        return $this->render('account/index.html.twig');
+        $orders = $orderRepo->findBy(['isPaid' => true, 'user'=>$this->getUser()],['id'=>'DESC']);
+
+        return $this->render('account/index.html.twig', [
+            'orders' => $orders,
+        ]);
+    }
+
+    #[Route('/order/{id}', name: 'account_order_details')]
+    public function show(?Order $order): Response
+    {
+        if(!$order || $order->getUser() !== $this->getUser()){
+            return $this->redirectToRoute("home");
+        }
+        return $this->render('account/detail_order.html.twig', [
+            'order' => $order,
+        ]);
     }
 }
